469240b67f4d44bd0c6e180f3021227ba88d6e48
sozoia <zfzg2232@gmail.com>
2023-11-07 18:35:26 +0300

    set main.c & main.h

---
+++ main.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc:
+ * @argv:
+ * @env:
+ * Return: 0
+*/
+int main(int argc, char *argv[], char *env[])
+{
+	char *arr = NULL;
+	size_t co = 0;
+	char prom[1] = "$";
+	char **command_token = NULL;
+
+	(void)argc;
+	(void)argv;
+	while (1) 
+	{
+		if(isatty(STDIN_FILENO) != 0)
+			write(1,&prom,1);
+
+		if (getline(&arr, &co, stdin) == -1)
+			exit (0);
+
+		command_token = token(arr);
+		if (command_token) 
+		{
+			control(command_token,env);
+			free_command(command_token);
+			free(arr);
+		}
+		else
+			free(arr);
+
+		arr = NULL;
+	}
+		return (0);
+}
