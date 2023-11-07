6133cc8781409a4aa38819f25e208afa6597c4f1
sozoia <zfzg2232@gmail.com>
2023-11-07 23:53:07 +0300

    did nothing

--- execfun.c
+++ execfun.c
@@ -2,30 +2,31 @@
 
 void execfun(char **args)
 {
-        int status;
-        pid_t pid;
+	int status;
+	pid_t pid;
 
-        if (strcmp(args[0], "cd") == 0)
-                cdfun(args);
-        else if (strcmp(args[0], "exit") == 0)
-                exitfun(args);
+	if (strcmp(args[0], "cd") == 0)
+		cdfun(args);
 
-        pid = fork();
-        if (pid > 0)
-        {
-                do
-                {
-                        waitpid(pid, &status, WUNTRACED);
-                } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
-        else if (pid == 0)
-        {
-                execvp(args[0], args);
-                perror("./shell: pid = 0");
-                exit(1);
-        }
-        else
-        {
-                perror("./shell: pid < 0");
-        }
+	else if (strcmp(args[0], "exit") == 0)
+		exitfun(args);
+
+	pid = fork();
+	if (pid > 0)
+	{
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	else if (pid == 0)
+	{
+		execvp(args[0], args);
+		perror("./shell: pid = 0");
+		exit(1);
+	}
+	else
+	{
+		perror("./shell: pid < 0");
+	}
 }
