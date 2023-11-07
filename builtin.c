ba308615b9873877ae7427e522f8902c697d8f19
sozoia <zfzg2232@gmail.com>
2023-11-07 18:58:16 +0300

    set control.c & builtin.c

---
+++ builtin.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+ * changedir - this is impelemntaion of the builtin "cd" command.
+ * @command: an array of string contain the command cd with the new WD.
+ * Return: 0 if success  else -1.
+*/
+int changedir(char *command[])
+{
+	if (chdir(command[1]) == -1)
+	{
+		perror("cd");
+		return(-1);
+	}
+	return(0);
+}
+
+/**
+ * exit_program this is impelemntaion of the builtin "exit" command.
+ * @command: an array of string contain the command exit.
+ * Return: 0 if success  else -1.
+*/
+int exit_program(char *command[])
+{
+	free(command);
+	exit(0);
+}
+/**
+ * which_builtin -  this function check if the enterd command is a builtin command or not.
+ * @command: an array of string contain the command.
+ * Return: the number of the command in array i have in certain order if success  else -1.
+ */
+int which_builtin(char *command[])
+{
+	int i;
+	const char *builtins[] = {"cd", "exit", NULL};
+
+	for (i = 0; builtins[i] != NULL; i++) 
+	{
+		if (strcmp(builtins[i], command[0]) == 0) 
+			return (i);
+	}
+	return (-1);
+}
+/**
+ * excute_builtin -  if the entered command is builtin it will choose the rigth function
+ * to implement that command
+ * @command: an array of string contain the builtin command.
+ * Return: 0 if success  else -1.
+ */
+int excute_builtin(char *command[])
+{
+	int i,r;
+	i = which_builtin(command);
+
+	switch(i)
+	{
+	case(0):
+		r = changedir(command);
+		return(r);
+	case(1):
+		r = exit_program(command);
+		return (r);
+	default:
+		return(-1);
+		break;
+	}
+
+	return (-1);
+}
---
+++ control.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * control - this function responsible of what will be the next step
+ *  after entering the command.
+ * @command_token: is an array of string contain command after tokenization.
+ * @env: array of string contain the enviroment variables
+ * Return: 0 if the command excuted seccessfully else return -1
+*/
+int control(char *command_token[],char *env[])
+{
+	int bu = -1;
+    	int ex = -1;
+	int check;
+
+
+    	if(!command_token[0])
+		return 0;
+
+	check = which_builtin(command_token);
+	if (check != -1)
+		bu = excute_builtin(command_token);
+
+	if (bu == -1 && check == -1)
+		ex = execute(command_token, env);
+
+	if (ex == -1)
+		return -1;
+
+	return 0;
+}
